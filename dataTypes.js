a49eb603c2e749f959ddd24cbaee1b056bbab7e2
dharmendra <[karkidharmendra21@gmail.com]>
2024-07-21 18:41:00 +0545

    datatypes

---
+++ dataTypes.js
@@ -0,0 +1,19 @@
+// alert( 5 + 5 ) // we are using node js not browser
+console.log("Dharmendra")
+let name ="dharmendra"
+let age =21
+let isLoggedIn = false
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+console.log(typeof 2132); //number
