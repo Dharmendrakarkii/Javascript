f5e55bfba7077067909d05a7fe6b6b5afd481f4a
dharmendra <[karkidharmendra21@gmail.com]>
2024-07-23 23:37:36 +0545

    add

--- 01_basics/conversionOperation.js
+++ 01_basics/conversionOperation.js
@@ -22,5 +22,33 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 11
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+
+let str1 = "hello"
+let str2 = " nitin "
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
