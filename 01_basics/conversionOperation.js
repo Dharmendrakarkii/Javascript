99778ceda36f550c83841a82ca1d221182f58460
dharmendra <[karkidharmendra21@gmail.com]>
2024-07-23 23:56:04 +0545

    add

--- 01_basics/conversionOperation.js
+++ 01_basics/conversionOperation.js
@@ -1,10 +1,10 @@
 let score ="hello"
-// console.log(typeof score);
-// console.log (typeof(score));
+console.log(typeof score);
+console.log (typeof(score));
 
 let valueInNumber = Number(score);
-// console.log(typeof valueInNumber);
-// console.log(valueInNumber);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
 
 // "11" => 11
 // "33abc" => NaN
@@ -13,7 +13,7 @@ let valueInNumber = Number(score);
 let isLoggedIn = "kapil"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-//  console.log(booleanIsLoggedIn);
+ console.log(booleanIsLoggedIn);
  
 // 1 => true; 0 => false
 // "" => false
@@ -22,21 +22,21 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 11
 
 let stringNumber = String(someNumber)
-// console.log(stringNumber);
-// console.log(typeof stringNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
 
 // *********************** Operations ***********************
 
 let value = 3
 let negValue = -value
-// console.log(negValue);
+console.log(negValue);
 
 
 let str1 = "hello"
 let str2 = " nitin "
 
 let str3 = str1 + str2
-// console.log(str3);
+console.log(str3);
 
 // console.log("1" + 2);
 // console.log(1 + "2");
@@ -45,8 +45,7 @@ let str3 = str1 + str2
 
 // console.log( (3 + 4) * 5 % 3);
 
-// console.log(+true);
-// console.log(+"");
+//
 
 
 let gameCounter = 100
