2b32f9a559222e38a4dc4d49e3335ef5f52218ba
dharmendra <[karkidharmendra21@gmail.com]>
2024-07-23 23:08:56 +0545

    conversion  of datatypes

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,26 @@
+let score ="hello"
+// console.log(typeof score);
+// console.log (typeof(score));
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "11" => 11
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = "kapil"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+ 
+// 1 => true; 0 => false
+// "" => false
+// "kapil" => true
+
+let someNumber = 11
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
