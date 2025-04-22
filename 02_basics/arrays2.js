4738bfc2706b0512e16cc512541c816c600b3691
Dharmendra karki <167868400+Dharmendrakarkii@users.noreply.github.com>
2025-04-22 14:18:19 +0545

    Add files via upload

--- 02_basics/arrays2.js
+++ 02_basics/arrays2.js
@@ -13,7 +13,39 @@ const dc_heros = ["superman","flash","batman"]
 //console.log(all_Heros);
 
 const all_new_heros = [...marvel_heros,  ...dc_heros]//spread method and concat are same but we prefer spread because it is easy to write a code
+
 console.log(all_new_heros);
+
+
+
+const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+
+const real_another_array =  another_array.flat(Infinity)
+
+console.log(real_another_array);
+
+
+console.log(Array.isArray("kapil"))
+
+console.log(Array.from("kapil"))
+
+console.log(Array.from({name: "kapil"}))//interesting
+
+
+
+
+
+let score1 = 100
+
+let score2 = 200
+
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3));
+
+
+
+
 
 
 
